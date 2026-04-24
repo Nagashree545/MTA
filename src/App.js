69cdfd549602e5904a4401150a5f20a41626c3d6
Nagashree-cws <Nagashree@creativewebsolution.in>
2026-04-24 18:34:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import Navigation from './Components/Header';
 import HeroSection from './Components/Herosection';
-import CoursesSection from './Components/CoursesSection';
 import WhyUsSection from './Components/WhyChooseUs';
-import InstructorsSection from './Components/Intersectors';
 import TestimonialsSection from './Components/Testimonial';
 import BlogSection from './Components/BlogSection';
-import PricingSection from './Components/Pricing';
 import ContactSection from './Components/Contact';
 import Footer from './Components/Footer';
 import StatsSection from './Components/StatsSction';
@@ -19,14 +16,11 @@ function App() {
       <Navigation/>
       <HeroSection/>
       <StatsSection/>
-      {/* <CoursesSection /> */}
       <WhyUsSection />
       <WhoCanEnroll/>
       <PopularCourses/>
-      {/* <InstructorsSection /> */}
       <TestimonialsSection/>
       <BlogSection />
-      {/* <PricingSection /> */}
       <ContactSection />
       <Footer />
     </div>
